47e497613afdfd52299722aa1f2ce168edb59053
LRapier <98057284+LRapier@users.noreply.github.com>
2022-02-07 13:14:37 -0700

    HeroBornV4

---
+++ Assets/AmmoPickup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.parent.gameObject);
+            Debug.Log("Ammo Pickup collected!");
+        }
+    }
+}
